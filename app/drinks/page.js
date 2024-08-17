2c67ffa243d8caee0167beda91760978f04782c7
vineetverma3000 <vineetverma3000@gmail.com>
2024-08-17 20:20:41 +0530

    adde home page

---
+++ app/drinks/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DrinksPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">DrinksPage</h1>
+    </div>
+  )
+}
+
+export default DrinksPage
---
+++ app/task/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">TaskPage</h1>
+    </div>
+  )
+}
+
+export default TaskPage
