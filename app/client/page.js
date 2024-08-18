de86248b4b2b16e3f7f5cc8b0046e7d7bc332768
vineetverma3000 <vineetverma3000@gmail.com>
2024-08-18 15:53:37 +0530

    added drinks page

--- app/client/page.js
+++ app/client/page.js
@@ -2,10 +2,14 @@
 import React, { useState } from 'react'
 
 const ClientPage = () => {
-  const [counter, setCounter] = useState(0)
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      <h1 className="text-7xl">ClientPage</h1>
+      <h1 className="text-7xl font-bold mb-4">{count}</h1>
+      <button className="btn btn-primary " onClick={() => setCount(count + 1)}>
+        click me
+      </button>
     </div>
   )
 }
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,6 +1,19 @@
 import React from 'react'
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 
-const DrinksPage = () => {
+const fetchDrinks = async (url) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
+
+const DrinksPage = async () => {
+  // const respone = await fetch(url)
+  // const data = await respone.json()
+  // console.log(data)
+  const data = await fetchDrinks(url)
+  console.log(data)
   return (
     <div>
       <h1 className="text-7xl">DrinksPage</h1>
