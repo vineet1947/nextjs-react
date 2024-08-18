87e140c8b7b61cb928416f36ad88cc2104da74b0
vineetverma3000 <vineetverma3000@gmail.com>
2024-08-18 18:13:13 +0530

    auth adde

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 
 const fetchDrinks = async (url) => {
-   await new Promise((resolve) => setTimeout(resolve, 2000))
+  await new Promise((resolve) => setTimeout(resolve, 2000))
   const response = await fetch(url)
   if (!response.ok) {
     throw new Error('Failed to fetch drinks')
