4eb2357909628fa36cbf669bb678a242a25cbd35
vineetverma3000 <vineetverma3000@gmail.com>
2024-08-18 15:33:32 +0530

    stable commit

--- app/about/info/page.js
+++ app/about/info/page.js
@@ -3,8 +3,8 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">HomePage</h1>
-      <Link href="/about" className="text-2xl">
+      <h1 className="text-7xl mb-8 font-bold">HomePage</h1>
+      <Link href="/" className="text-2xl">
         about page
       </Link>
     </div>
--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">HomePage</h1>
-      <Link href="/about" className="text-2xl">
-        about page
+      <h1 className="text-5xl mb-8 font-bold">Next.js Demo</h1>
+      <Link href="/client" className="btn btn-accent">
+        get started
       </Link>
     </div>
   )
